41510fe14b58c99a4056b44e64442027da117df1
aswinaashok <aswinaashok77@gmail.com>
2023-08-09 10:14:47 +0530

    experience

--- client/src/components/RecruiterDropDown/whyRecruit.jsx
+++ client/src/components/RecruiterDropDown/whyRecruit.jsx
@@ -1,11 +1,104 @@
-import React from "react";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
-const whyRecruit = () => {
-    return (
-        <div>
-            <h1>Why Recruit ??</h1>
-        </div>
-    );
+const WhyRecruit = () => {
+  return (
+    <section id="why-recruit" style={sectionStyle}>
+      <Container>
+        <h2 style={headingStyle} className="text-center mb-4">
+          Why College of Engineering Trivandrum?
+        </h2>
+        <hr></hr>
+        <br></br>
+        <Row>
+          <Col md={6} style={columnStyle} className="mb-4">
+            <div style={itemStyle}>
+              <h4 style={titleStyle}>Quality Education and Excellence</h4>
+              <p style={descriptionStyle}>
+                College of Engineering Trivandrum (CET) is renowned for its commitment to providing top-notch engineering education. Our rigorous academic programs are designed to foster critical thinking, problem-solving skills, and creativity among our students.
+              </p>
+            </div>
+          </Col>
+          <Col md={6} style={columnStyle} className="mb-4">
+            <div style={itemStyle}>
+              <h4 style={titleStyle}>Talented and Diverse Student Pool</h4>
+              <p style={descriptionStyle}>
+                Our diverse student body brings together some of the brightest minds from across the country. CET students are known for their dedication, technical expertise, and innovative mindset, making them valuable assets to any organization.
+              </p>
+            </div>
+          </Col>
+          <Col md={6} style={columnStyle} className="mb-4">
+            <div style={itemStyle}>
+              <h4 style={titleStyle}>Industry-Focused Curriculum</h4>
+              <p style={descriptionStyle}>
+              Our curriculum is designed in close collaboration with industry experts to ensure that our graduates are well-prepared to meet the demands of the modern workforce. Our students acquire practical skills and knowledge that are directly applicable to real-world challenges.
+              </p>
+            </div>
+          </Col>
+          <Col md={6} style={columnStyle} className="mb-4">
+            <div style={itemStyle}>
+              <h4 style={titleStyle}>State-of-the-Art Facilities</h4>
+              <p style={descriptionStyle}>
+              CET boasts state-of-the-art labs, research facilities, and technology resources that empower students to explore and innovate. Our modern campus environment facilitates hands-on learning, research, and development in various engineering disciplines.
+              </p>
+            </div>
+          </Col>
+          <Col md={6} style={columnStyle} className="mb-4">
+            <div style={itemStyle}>
+              <h4 style={titleStyle}>Talented and Diverse Student Pool</h4>
+              <p style={descriptionStyle}>
+                Our diverse student body brings together some of the brightest minds from across the country. CET students are known for their dedication, technical expertise, and innovative mindset, making them valuable assets to any organization.
+              </p>
+            </div>
+          </Col>
+          <Col md={6} style={columnStyle} className="mb-4">
+            <div style={itemStyle}>
+              <h4 style={titleStyle}>Strong Alumni Network</h4>
+              <p style={descriptionStyle}>
+              Our extensive alumni network consists of successful professionals who have made significant contributions to various industries. Many of our alumni hold leadership positions in renowned companies and organizations, providing a strong support system for our graduates.
+              </p>
+            </div>
+          </Col>
+          {/* Add more columns for other features */}
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
-export default whyRecruit;
+export default WhyRecruit;
+
+// Inline styles
+const sectionStyle = {
+  backgroundColor: '#f8f9fa',
+  padding: '80px 0',
+};
+
+const headingStyle = {
+  fontSize: '36px',
+  color: '#20313e',
+  fontWeight: 'bold',
+};
+
+const columnStyle = {
+  marginBottom: '20px',
+};
+
+const itemStyle = {
+  backgroundColor: '#ffffff',
+  padding: '30px',
+  borderRadius: '8px',
+  boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.1)',
+  transition: 'transform 0.3s, box-shadow 0.3s',
+};
+
+const titleStyle = {
+  fontSize: '24px',
+  color: '#20313e',
+  marginBottom: '20px',
+};
+
+const descriptionStyle = {
+  fontSize: '16px',
+  color: '#6c757d',
+};
--- client/src/components/studentDropdown/placementExperience.jsx
+++ client/src/components/studentDropdown/placementExperience.jsx
@@ -1,11 +1,164 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Container, Row, Col, Card, ListGroup } from "react-bootstrap";
 
-const placementExperience = () => {
+const PlacementExperience = () => {
+  const experiences = [
+    {
+      company: "Google",
+      name: "Manjunath",
+      role: "Software Engineer",
+      duration: "May 2022 - Present",
+      description:
+        "Worked on developing web applications and implementing new features.",
+      linkedin: "https://www.linkedin.com/in/your-linkedin-profile",
+      instagram: "https://www.instagram.com/your-instagram-id",
+    },
+    {
+      name: "Akshay",
+      company: "Texas Instruments",
+      role: "Intern",
+      duration: "January 2022 - April 2022",
+      description: "Assisted in testing and debugging software products.",
+      linkedin: "https://www.linkedin.com/in/your-linkedin-profile",
+      instagram: "https://www.instagram.com/your-instagram-id",
+    },
+    {
+      name: "Midhun",
+      company: "Amadeus",
+      role: "Software Enginner",
+      duration: "January 2022 - Present",
+      description: "Assisted in testing and debugging software products.",
+      linkedin: "https://www.linkedin.com/in/your-linkedin-profile",
+      instagram: "https://www.instagram.com/your-instagram-id",
+    },
+    {
+      name: "Kuttan",
+      company: "Key value Solutions",
+      role: "Software Engineer",
+      duration: "May 2023 - Present",
+      description:
+        "Worked on developing web applications and implementing new features.",
+      linkedin: "https://www.linkedin.com/in/your-linkedin-profile",
+      instagram: "https://www.instagram.com/your-instagram-id",
+    },
+    {
+      name: "Chikku",
+      company: "Open Fintech",
+      role: "Software Engineer",
+      duration: "May 2022 - Present",
+      description:
+        "Worked on developing web applications and implementing new features.",
+      linkedin: "https://www.linkedin.com/in/your-linkedin-profile",
+      instagram: "https://www.instagram.com/your-instagram-id",
+    },
+    {
+      name: "Aju",
+      company: "Oracle",
+      role: "Intern",
+      duration: "January 2022 - April 2022",
+      description: "Assisted in testing and debugging software products.",
+      linkedin: "https://www.linkedin.com/in/your-linkedin-profile",
+      instagram: "https://www.instagram.com/your-instagram-id",
+    },
+
+    // Add more experience objects as needed
+  ];
+
+  const [selectedCompany, setSelectedCompany] = useState(null);
+  const [expandedIndex, setExpandedIndex] = useState(null);
+
+  const handleCompanyClick = (company) => {
+    setSelectedCompany(company);
+    setExpandedIndex(null); // Reset the expanded index when a company is clicked
+  };
+
+  const handleExpandToggle = (index) => {
+    setExpandedIndex(expandedIndex === index ? null : index);
+  };
+
+  // Filter experiences based on selected company
+  const filteredExperiences = selectedCompany
+    ? experiences.filter((experience) => experience.company === selectedCompany)
+    : experiences;
+
   return (
-    <div style={{height:"100vh", margin:"10px"}}>
-        <h1>Placement Experience</h1>
-    </div>
-  )
-}
+    <section id="placement-experience" className="py-5 bg-light">
+      <Container>
+        <h2
+          className="text-center mb-4"
+          style={{ fontSize: "50px", color: "#20313e", fontWeight: "bold" }}
+        >
+          Placement Experience
+        </h2>
+        <h4 className="text-center mb-4">Experience matters...</h4>
+        <hr></hr>
+        <br></br>
+        <Row>
+          <Col md={3}>
+            <ListGroup>
+              <ListGroup.Item action onClick={() => handleCompanyClick(null)}>
+                All Companies
+              </ListGroup.Item>
+              {experiences.map((experience, index) => (
+                <ListGroup.Item
+                  key={index}
+                  action
+                  onClick={() => handleCompanyClick(experience.company)}
+                >
+                  {experience.company}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col md={9}>
+            {filteredExperiences.map((experience, index) => (
+              <Card key={index} className="mb-3">
+                <Card.Body>
+                  <Card.Title>
+                    {experience.name} - {experience.company}
+                  </Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    {experience.role}
+                  </Card.Subtitle>
+                  <Card.Text>{experience.duration}</Card.Text>
+                  {expandedIndex === index ? (
+                    <>
+                      <Card.Text>{experience.description}</Card.Text>
+                      <div className="mb-2">
+                        <a
+                          href={experience.linkedin}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          LinkedIn Profile
+                        </a>
+                      </div>
+                      <div>
+                        <a
+                          href={experience.instagram}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Instagram ID
+                        </a>
+                      </div>
+                      <a href="#" onClick={() => handleExpandToggle(index)}>
+                        Read Less
+                      </a>
+                    </>
+                  ) : (
+                    <a href="#" onClick={() => handleExpandToggle(index)}>
+                      Read More
+                    </a>
+                  )}
+                </Card.Body>
+              </Card>
+            ))}
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
 
-export default placementExperience
+export default PlacementExperience;
