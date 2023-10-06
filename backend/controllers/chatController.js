feecab0e05073c4c95151ae1ee4d3f2311783800
Aswin A Ashok <79050321+aswinaashok@users.noreply.github.com>
2023-10-06 07:08:39 +0530

    chats

---
+++ backend/controllers/chatController.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const chatController = () => {
+  return (
+    <div>chatController</div>
+  )
+}
+
+export default chatController
---
+++ backend/routes/chatRoute.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const chatRoute = () => {
+  return (
+    <div>chatRoute</div>
+  )
+}
+
+export default chatRoute
