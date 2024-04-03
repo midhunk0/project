4a6fb590debe392d50558f8efe4645a64988dd7e
Aswin A Ashok <79050321+aswinaashok@users.noreply.github.com>
2024-04-03 22:36:47 +0530

    admin dashboard and invitations design

--- client/src/components/admin/AdminDashboard.js
+++ client/src/components/admin/AdminDashboard.js
@@ -1,17 +1,40 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Grid } from "@mui/material";
 
-const AdminDashboard=()=>{
-    return (
-        <Box width="75%" margin="10px" border="1px solid gray" padding="10px" height="100%">
-            <Typography variant="h4" >Admin Name: Dr. K. Sunil Kumar</Typography>
-            <Typography variant="h5" >Training and Placement Officer <br/>
-
-College of Engineering Trivandrum
-                </Typography>
-            
+const AdminDashboard = () => {
+  return (
+    <Grid container spacing={3} justifyContent="center" mt="50px">
+      <Grid item xs={12} sm={8} md={6}>
+        <Box
+          width="100%"
+          padding="20px"
+          border="1px solid gray"
+          borderRadius="10px"
+          boxShadow="0 4px 8px rgba(0, 0, 0, 0.1)"
+          bgcolor="white"
+        >
+            <img
+            src="../../assets/admin.jpg"
+            alt="admin_image"
+            style={{
+              width: "100%",
+              height: "200px",
+              objectFit: "cover",
+              borderRadius: "5px",
+            }}
+          />
+          <Typography variant="h4" gutterBottom marginTop={3}>
+            Dr. K. Sunil Kumar
+          </Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            Training and Placement Officer
+            <br />
+            College of Engineering Trivandrum
+          </Typography>
         </Box>
-    );
+      </Grid>
+    </Grid>
+  );
 };
 
 export default AdminDashboard;
--- client/src/components/admin/Invitations.js
+++ client/src/components/admin/Invitations.js
@@ -1,106 +1,126 @@
-import { Box, Button, Typography, styled } from "@mui/material";
+import { Box, Button, Typography, styled, Grid } from "@mui/material";
 import React, { useState } from "react";
 import * as XLSX from "xlsx";
 import { tokens } from "../../theme";
 import CssTextField from "../../components/global/CssTextField";
 import toast, { Toaster } from "react-hot-toast";
 
-const colors = tokens();
 const UploadExcel = () => {
-    const [file, setFile] = useState(null);
+  const [file, setFile] = useState(null);
 
-    const handleFileChange = (e) => {
-        const selectedFile = e.target.files[0];
-        setFile(selectedFile);
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
 
-        const reader = new FileReader();
+    const reader = new FileReader();
 
-        reader.onload = (event) => {
-            const data = new Uint8Array(event.target.result);
-            const workbook = XLSX.read(data, { type: "array" });
+    reader.onload = (event) => {
+      const data = new Uint8Array(event.target.result);
+      const workbook = XLSX.read(data, { type: "array" });
 
-            const jsonData = XLSX.utils.sheet_to_json(
-                workbook.Sheets[workbook.SheetNames[0]]
-            );
-            console.log(jsonData);
+      const jsonData = XLSX.utils.sheet_to_json(
+        workbook.Sheets[workbook.SheetNames[0]]
+      );
+      console.log(jsonData);
 
-            // Send each user from the jsonData array to the server for registration
-            const requests = jsonData.map((user) =>
-                fetch("http://localhost:8080/api/students/studentRegister", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(user),
-                })
-                    .then((response) => {
-                        if (!response.ok) {
-                            throw new Error("Network response was not ok");
-                        }
-                        return response.json();
-                    })
-                    .then((data) => {
-                        console.log(data);
-                        // Handle the response from the server
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                        // Handle any errors
-                    })
-            );
+      // Send each user from the jsonData array to the server for registration
+      const requests = jsonData.map((user) =>
+        fetch("http://localhost:8080/api/students/studentRegister", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data);
+            // Handle the response from the server
+          })
+          .catch((error) => {
+            console.log(error);
+            // Handle any errors
+          })
+      );
 
-            console.log(requests);
+      console.log(requests);
 
-            Promise.all(requests)
-                .then(() => {
-                    console.log("All users registered successfully");
-                })
-                .catch((error) => {
-                    toast.error("Error registering users: " + error.message);
-                });
-        };
-
-        reader.readAsArrayBuffer(selectedFile);
+      Promise.all(requests)
+        .then(() => {
+          console.log("All users registered successfully");
+        })
+        .catch((error) => {
+          toast.error("Error registering users: " + error.message);
+        });
     };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    reader.readAsArrayBuffer(selectedFile);
+  };
 
-        if (!file) {
-            console.log("No file selected");
-            toast.error("No file selected");
-            return;
-        } else {
-            toast.success("All users registered successfully");
-        }
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    return (
-        <Box margin="20px" width="100%">
-            <Typography variant="h4">Upload Excel File</Typography>
-            <Box marginTop="10px">
-                <CssTextField
-                    type="file"
-                    onChange={handleFileChange}
-                    inputProps={{
-                        accept: ".xlsx, .xls",
-                    }}
-                />
-                <Button
-                    variant="contained"
-                    sx={{
-                        background: colors.gray[100],
-                        "&:hover": { background: colors.gray[100] },
-                        margin: "10px",
-                    }}
-                    onClick={handleSubmit}
-                >
-                    Upload
-                </Button>
-            </Box>
-            <Toaster position="bottom-center" /> {/* Add the toast container */}
+    if (!file) {
+      console.log("No file selected");
+      toast.error("No file selected");
+      return;
+    } else {
+      toast.success("All users registered successfully");
+    }
+  };
+
+  return (
+    <Grid container spacing={3} justifyContent="center" margin="20px">
+      <Grid item xs={10} sm={5} md={4}>
+        <Box
+          boxShadow={3}
+          padding="20px"
+          border="1px solid gray"
+          borderRadius="5px"
+          bgcolor="white"
+        >
+          <img
+            src="../../assets/excel2.png"
+            alt="Company_image"
+            style={{
+              width: "100%",
+              height: "270px",
+              objectFit: "cover",
+              borderRadius: "5px",
+            }}
+          />
+          <Typography variant="h4" margin="20px">
+            Upload Excel File
+          </Typography>
+          <CssTextField
+            type="file"
+            onChange={handleFileChange}
+            inputProps={{
+              accept: ".xlsx, .xls",
+            }}
+          />
+          <Button
+            variant="contained"
+            onClick={handleSubmit}
+            sx={{
+              bgcolor: "#4CAF50", // Green color
+              "&:hover": {
+                bgcolor: "#388E3C", // Darker green color on hover
+              },
+              margin: "10px",
+            }}
+          >
+            Upload
+          </Button>
         </Box>
-    );
+      </Grid>
+    </Grid>
+  );
 };
 
 export default UploadExcel;
